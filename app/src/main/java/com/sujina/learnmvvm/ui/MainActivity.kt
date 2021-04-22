bf04ebc113631354ec90efb0bcb5ebd79043004f
Ahmed BARGADY <bargadyahmed2000@gmail.com.com>
2021-04-22 17:49:58 +0000

    creating and connecting the navigation graph to our fragments.

--- app/src/main/java/com/sujina/learnmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/sujina/learnmvvm/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sujina.learnmvvm
+package com.sujina.learnmvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sujina.learnmvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/sujina/learnmvvm/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.sujina.learnmvvm.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.sujina.learnmvvm.R
+
+class TasksFragment: Fragment(R.layout.fragment_tasks) {
+}
