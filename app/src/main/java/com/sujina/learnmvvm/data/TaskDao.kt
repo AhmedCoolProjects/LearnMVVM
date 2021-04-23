dca462e474834574cab7bf06f4ec9914dc10d1c8
Ahmed BARGADY <bargadyahmed2000@gmail.com.com>
2021-04-23 06:54:45 +0000

    setting up the sort functionalities, and the hide completed tasks function.

--- app/src/main/java/com/sujina/learnmvvm/data/TaskDao.kt
+++ app/src/main/java/com/sujina/learnmvvm/data/TaskDao.kt
@@ -1,12 +1,23 @@
 package com.sujina.learnmvvm.data
 
 import androidx.room.*
+import com.sujina.learnmvvm.ui.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
-    @Query("SELECT * FROM task_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
-    fun getTasks(searchQuery: String): Flow<List<Task>>
+
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when (sortOrder) {
+            SortOrder.BY_DATE -> getTasksSortedByDateCreated(query, hideCompleted)
+            SortOrder.BY_NAME -> getTasksSortedByName(query, hideCompleted)
+        }
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompletd OR completed = 0 ) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name")
+    fun getTasksSortedByName(searchQuery: String, hideCompletd: Boolean): Flow<List<Task>>
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompletd OR completed = 0 ) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, created")
+    fun getTasksSortedByDateCreated(searchQuery: String, hideCompletd: Boolean): Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task)
