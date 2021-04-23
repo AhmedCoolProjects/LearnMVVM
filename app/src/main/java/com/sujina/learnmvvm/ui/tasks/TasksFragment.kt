6f74a821662ee5963588cb9c2755f51ed2bd30b2
Ahmed BARGADY <bargadyahmed2000@gmail.com.com>
2021-04-23 06:30:32 +0000

    setting the search functionality.

--- app/src/main/java/com/sujina/learnmvvm/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/sujina/learnmvvm/ui/tasks/TasksFragment.kt
@@ -1,12 +1,17 @@
 package com.sujina.learnmvvm.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sujina.learnmvvm.R
 import com.sujina.learnmvvm.databinding.FragmentTasksBinding
+import com.sujina.learnmvvm.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tasks.*
 
@@ -32,5 +37,42 @@ class TasksFragment: Fragment(R.layout.fragment_tasks) {
         viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
         }
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_sort_by_name -> {
+
+                true
+            }
+            R.id.action_sort_by_date -> {
+
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+
+                true
+            }
+            R.id.action_delete_completed_tasks -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
+
+
